6313f8940033702633004f48a0a1189fbff60649
Nigel Rodrigues <nigel1@ualberta.ca>
2018-09-22 11:51:57 -0400

    add event listener for the active tab

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-   chrome.declarativeContent.onPageChanged.addRules([{
-     conditions: [new chrome.declarativeContent.PageStateMatcher({
-       pageUrl: {schemes: ['https', 'http', 'localhost', 'chrome', 'file']},
-     })
-     ],
-         actions: [new chrome.declarativeContent.ShowPageAction()]
-   }]);
- });
-
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {schemes: ['https', 'http', 'localhost', 'chrome', 'file']},
+    })
+  ],
+  actions: [new chrome.declarativeContent.ShowPageAction()]
+}]);
+});
+chrome.pageAction.onClicked.addListener(function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {code:  'document.body.style.backgroundColor = "green";' }
+    );
+  })
+});
