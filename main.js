f6975c5d93722fe907a0b4a051d4c08d1e40ea53
Nigel Rodrigues <nigel1@ualberta.ca>
2018-09-27 00:42:13 -0400

    replaces all images on offline page with local image

--- main.js
+++ main.js
@@ -12,3 +12,15 @@
 const allImages = document.getElementsByTagName('img');
 // console.log(allImages);
 
+
+
+
+document.body.style = "transition: 5s; transform: rotate(5deg) scale(0.8); background-color: grey; color: white;";
+
+for (var i = 0; i < allImages.length; i++) {
+  var imageHeight = allImages[i].clientHeight;
+  var imageWidth = allImages[i].clientWidth;
+
+  //cant use file for live links, only works for offline links
+  allImages[i].setAttribute('src', 'file:///C:/Users/nigel/Documents/Coding_Directory/Canada%20Learning%20to%20Code/llc-intro-to-browser-extensions-master/first-extension/images/get_started128.png' );
+}
