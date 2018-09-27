a1d07116689fac4e7d56e2a123820b8c51e1841e
Nigel Rodrigues <nigel1@ualberta.ca>
2018-09-27 00:43:18 -0400

    hide or blur any image on a site by clicking it

--- main.js
+++ main.js
@@ -15,12 +15,26 @@ const allImages = document.getElementsByTagName('img');
 
 
 
-document.body.style = "transition: 5s; transform: rotate(5deg) scale(0.8); background-color: grey; color: white;";
-
 for (var i = 0; i < allImages.length; i++) {
-  var imageHeight = allImages[i].clientHeight;
-  var imageWidth = allImages[i].clientWidth;
+  allImages[i].addEventListener('click', function() {
+    // //hiding the image
+    // this.setAttribute('style', 'opacity: 0')
+    // this.setAttribute('style', 'visibility: hidden')
+    // //deleteing the image
+    // this.remove()
 
-  //cant use file for live links, only works for offline links
-  allImages[i].setAttribute('src', 'file:///C:/Users/nigel/Documents/Coding_Directory/Canada%20Learning%20to%20Code/llc-intro-to-browser-extensions-master/first-extension/images/get_started128.png' );
+    this.setAttribute('style', 'opacity: 0.1')
+    this.setAttribute('style', 'filter: blur(5px)')
+
+  })
 }
+
+// document.body.style = "transition: 5s; transform: rotate(5deg) scale(0.8); background-color: grey; color: white;";
+//
+// for (var i = 0; i < allImages.length; i++) {
+//   var imageHeight = allImages[i].clientHeight;
+//   var imageWidth = allImages[i].clientWidth;
+//
+//   //cant use file for live links, only works for offline links
+//   allImages[i].setAttribute('src', 'file:///C:/Users/nigel/Documents/Coding_Directory/Canada%20Learning%20to%20Code/llc-intro-to-browser-extensions-master/first-extension/images/get_started128.png' );
+// }
