47a07c200e105d88bffe334c05271853812ae2d7
Nigel Rodrigues <nigel1@ualberta.ca>
2018-09-27 00:39:27 -0400

    console log all elements on the page with id=main

--- main.js
+++ main.js
@@ -2,5 +2,9 @@
 // document.body.style.backgroundColor = "purple";
 
 // //changes css style of a the page
-document.body.style = "transition: 5s; transform: rotate(5deg) scale(0.8); background-color: grey; color: white;";
+// document.body.style = "transition: 5s; transform: rotate(5deg) scale(0.8); background-color: grey; color: white;";
+
+// // get all html nodes with id="main" on the webpage and console log it
+const allIds = document.getElementById("main");
+console.log(allIds);
 
