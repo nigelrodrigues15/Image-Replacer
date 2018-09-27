d0eb0ed1a191070d6c32751d1853b5c79280f46f
Nigel Rodrigues <nigel1@ualberta.ca>
2018-09-27 00:39:57 -0400

    get all images form the webpage

--- main.js
+++ main.js
@@ -5,6 +5,10 @@
 // document.body.style = "transition: 5s; transform: rotate(5deg) scale(0.8); background-color: grey; color: white;";
 
 // // get all html nodes with id="main" on the webpage and console log it
-const allIds = document.getElementById("main");
-console.log(allIds);
+// const allIds = document.getElementById("main");
+// console.log(allIds);
+
+// get all images on the webpage
+const allImages = document.getElementsByTagName('img');
+// console.log(allImages);
 
